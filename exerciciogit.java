2f75ac603a0c2fc5c0595021f5c9d3384ae3ad31
GabrielSavioPereira <113271446+GabrielSavioPereira@users.noreply.github.com>
2023-03-16 17:30:36 -0300

    Add files via upload

---
+++ exerciciogit.java
@@ -0,0 +1,18 @@
+package ExercicioGit;
+
+import java.util.Scanner;
+
+public class exerciciogit{
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+   
+    System.out.print("Digite o primeiro numero: ");
+   int n1 = scan.nextInt();
+   System.out.print("Digite o segundo numero: ");
+   int n2 = scan.nextInt();
+   System.out.println("A soma total é de: "+(n1+n2));
+   
+   }
+    
+
+    }
