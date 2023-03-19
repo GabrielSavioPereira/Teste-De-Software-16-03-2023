b0aae045653dc45759b7c648bf0c81a000d3ce8e
GabrielSavioPereira <113271446+GabrielSavioPereira@users.noreply.github.com>
2023-03-19 13:36:22 -0300

    Add files via upload

--- exerciciogit.java
+++ exerciciogit.java
@@ -10,13 +10,17 @@ public class exerciciogit {
             System.out.println("Digite o primeiro numero: ");
             int num1 = scan.nextInt();
 
+        try {
+            System.out.println("Digite o segundo numero: ");
+            int num2 = scan.nextInt();
+            System.out.println("A soma total é de:  " + (num1 + num2));
+
+            
         } catch (Exception e) {
             System.out.println("Usuario Digitou letra");
 
         }
-        try {
-            System.out.println("Digite o segundo numero: ");
-            int num2 = scan.nextInt();
+                                                              
         } catch (Exception e) {
             System.out.println("Usuario Digitou letra");
         }
